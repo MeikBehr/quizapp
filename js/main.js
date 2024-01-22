1cd4ddcf4f157bb01a34558328e071040fc6eccb
Meik Behr <meik.behr@gmx.de>
2024-01-22 18:04:47 +0100

    check answer + color answer + able/disable Button

--- js/main.js
+++ js/main.js
@@ -12,9 +12,30 @@ function init() {
 function showQuestion() {
 	let question = questions[currentQuestion];
 	document.getElementById('questionText').innerHTML = question['question'];
-	document.getElementById('answer_1').innerHTML = question['answer_1'];
+	document.getElementById('answer_1').innerHTML = question['answer_1'];  
 	document.getElementById('answer_2').innerHTML = question['answer_2'];
 	document.getElementById('answer_3').innerHTML = question['answer_3'];
 	document.getElementById('answer_4').innerHTML = question['answer_4'];
 	
+}
+
+
+function answer(selection) {
+	let question = questions[currentQuestion];
+	let selectedQuestionNumber = parseInt(selection.slice(-1));
+	let isAnswerCorrect = selectedQuestionNumber === question['right_answer'];
+	let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+	if (isAnswerCorrect) {
+		document.getElementById(selection).parentNode.classList.add('bg-success');
+	} 
+	else 
+	{
+		document.getElementById(selection).parentNode.classList.add('bg-danger');
+		document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+	}
+
+	document.getElementById('next-button').removeAttribute("disabled");
+	// document.getElementById('next-button').disabled = false;
+
 }
